1c458df43d2d1625ee8597fb7a1e77f22dfe4519
AlphaKretin <deewhydeeecks@gmail.com>
2021-06-27 16:17:19 +0930

    Shard using Discord.js shard manager

---
+++ src/bot.ts
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+dotenv.config();
+import debug from "debug";
+import { Client, ClientOptions, Guild, Intents } from "discord.js";
+
+export function serializeServer(server: Guild): string {
+    if ("name" in server) {
+        const createdAt = new Date(server.createdAt).toISOString();
+        return `${server.name} (${server.id}) [${server.memberCount}] ${createdAt} by <@${server.ownerID}>`;
+    } else {
+        return `${server.id}`;
+    }
+}
+
+const logger = debug("bot");
+const log = logger.extend("log");
+const warn = logger.extend("warn");
+const error = logger.extend("error");
+
+const clientOptions: ClientOptions = {
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+};
+
+const bot = new Client(clientOptions);
+
+bot.on("warn", message => warn(`Shard ${bot.shard}: ${message}`));
+bot.on("error", message => error(`Shard ${bot.shard}: ${message}`));
+bot.on("connect", shard => log(`Shard ${shard} connected to Discord`));
+bot.on("disconnect", () => log("Disconnected from Discord"));
+bot.on("shardReady", shard => log(`Shard ${shard} ready`));
+bot.on("shardDisconnect", shard => log(`Shard ${shard} disconnected`));
+bot.on("guildCreate", guild => log(`Guild create: ${serializeServer(guild)}`));
+bot.on("guildDelete", guild => log(`Guild delete: ${serializeServer(guild)}`));
+bot.on("ready", () => log(`Logged in as ${bot.user?.tag} - ${bot.user?.id}`));
+
+bot.login(`${process.env.DISCORD_TOKEN}`).catch(error);
+process.once("SIGTERM", () => bot.destroy());
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,15 @@
-import dotenv from "dotenv";
-dotenv.config();
-import debug from "debug";
-import { Client, ClientOptions, Guild } from "discord.js";
+import { debug } from "debug";
+import { ShardingManager } from "discord.js";
 
-export function serializeServer(server: Guild): string {
-    if ("name" in server) {
-        const createdAt = new Date(server.createdAt).toISOString();
-        return `${server.name} (${server.id}) [${server.memberCount}] ${createdAt} by <@${server.ownerID}>`;
-    } else {
-        return `${server.id}`;
-    }
-}
-
-const logger = debug("bot");
+const logger = debug("sharding");
 const log = logger.extend("log");
 const warn = logger.extend("warn");
 const error = logger.extend("error");
 
-const shard = parseInt(`${process.env.DISCORD_SHARD}`) - 1;
-const options = isNaN(shard)
-    ? {}
-    : {
-          firstShardID: shard,
-          lastShardID: shard
-      };
+const manager = new ShardingManager("./bot.js", {
+    token: `${process.env.DISCORD_TOKEN}`,
+    totalShards: parseInt(`${process.env.DISCORD_TOTAL_SHARDS}`) || "auto"
+});
 
-const clientOptions: ClientOptions = {};
-
-if (parseInt(`${process.env.DISCORD_TOTAL_SHARDS}`)) {
-    clientOptions.shardCount = parseInt(`${process.env.DISCORD_TOTAL_SHARDS}`);
-} else {
-    clientOptions.shards = "auto";
-}
-
-const bot = new Client(clientOptions);
-
-bot.on("warn", message => warn(`Shard ${bot.shard}: ${message}`));
-bot.on("error", message => error(`Shard ${bot.shard}: ${message}`));
-bot.on("connect", shard => log(`Shard ${shard} connected to Discord`));
-bot.on("disconnect", () => log("Disconnected from Discord"));
-bot.on("shardReady", shard => log(`Shard ${shard} ready`));
-bot.on("shardDisconnect", shard => log(`Shard ${shard} disconnected`));
-bot.on("guildCreate", guild => log(`Guild create: ${serializeServer(guild)}`));
-bot.on("guildDelete", guild => log(`Guild delete: ${serializeServer(guild)}`));
-bot.on("ready", () => log(`Logged in as ${bot.user?.tag} - ${bot.user?.id}`));
-
-bot.login(`${process.env.DISCORD_TOKEN}`).catch(error);
-process.once("SIGTERM", () => bot.destroy());
+manager.on("shardCreate", shard => log(`Shard ${shard} spawned.`));
+manager.spawn();
