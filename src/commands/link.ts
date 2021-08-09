375879acc586ddee72b4832334ad92eb9f2251a3
AlphaKretin <deewhydeeecks@gmail.com>
2021-08-09 14:31:29 +0930

    Fill out list of links
    Some links from old bastion are excluded due to being outdated or Ignis-specific

--- src/commands/link.ts
+++ src/commands/link.ts
@@ -16,6 +16,42 @@ export class LinkCommand extends Command {
 		lfocg: {
 			name: "OCG Banlist",
 			result: "https://www.yugioh-card.com/my/event/rules_guides/forbidden_cardlist.php?lang=en"
+		},
+		dubsum: {
+			name: "Double Summon List",
+			result: "https://ygorganization.com/doublesummonlist/"
+		},
+		forget: {
+			name: "Forgetting",
+			result: "https://ygorganization.com/learnrulingspart13/"
+		},
+		dmgstep: {
+			name: "Damage Step",
+			result: "https://www.yugioh-card.com/uk/gameplay/damage.html\nhttp://yugipedia.com/wiki/Damage_Step#Cards_and_effects_that_can_be_activated"
+		},
+		linksum: {
+			name: "Link Summons",
+			result: "https://cdn.discordapp.com/attachments/377682286394736650/690868673157791774/unknown.png"
+		},
+		fetc: {
+			name: "Fast Effect Timing Chart",
+			result: "https://img.yugioh-card.com/en/gameplay/images/T-Flowchart_EN-US.jpg"
+		},
+		sumneg: {
+			name: "Summon Negation Timing",
+			result: "https://puu.sh/CLinf/3332459d3c.png"
+		},
+		atkdef: {
+			name: "ATK/DEF Modification",
+			result: "https://ygorganization.com/atk-def-modification-and-you/"
+		},
+		nomi: {
+			name: "Special Summon Monsters",
+			result: "https://cdn.discordapp.com/attachments/184324960842416129/680508513105346659/nomi_monsters.png"
+		},
+		rivgoz: {
+			name: "Rivalry/Gozen Rulings",
+			result: "https://img.yugioh-card.com/ygo_cms/ygo/all/uploads/CardFAQ_Rivalry-of-Warlords_Gozen-Match-1.pdf"
 		}
 	};
 
