4b4d93ddaa54bd11503df73b0f3fc6a48421e50f
Luna Brand <deewhydeeecks@gmail.com>
2021-08-10 12:48:14 +0930

    Reimplement "link" command (#1)
    Some links from old bastion are excluded due to being outdated or Ignis-specific

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,63 +1,64 @@
 import { ApplicationCommandData, Client, ClientApplication, Guild, Snowflake } from "discord.js";
 import { HelpCommand } from "./help";
 import { PingCommand } from "./ping";
+import { LinkCommand } from "./link";
 
-export const classes = [HelpCommand, PingCommand];
-export { HelpCommand, PingCommand };
+export const classes = [HelpCommand, PingCommand, LinkCommand];
+export { HelpCommand, PingCommand, LinkCommand };
 
 interface LoggableCommandMetadata {
-    id: Snowflake;
-    name: string;
-    description: string;
+	id: Snowflake;
+	name: string;
+	description: string;
 }
 
 async function deployCommands(
-    api: Guild | ClientApplication,
-    commands: ApplicationCommandData[]
+	api: Guild | ClientApplication,
+	commands: ApplicationCommandData[]
 ): Promise<LoggableCommandMetadata[]> {
-    await api.commands.set(commands);
-    // This is a bug in TypeScript failing to resolve the correct overload! TODO report
-    const created =  await ("guild" in api.commands ? api.commands.fetch() : api.commands.fetch());
-    return created.map(command => ({
-        id: command.id,
-        name: command.name,
-        description: command.description
-    }));
+	await api.commands.set(commands);
+	// This is a bug in TypeScript failing to resolve the correct overload! TODO report
+	const created = await ("guild" in api.commands ? api.commands.fetch() : api.commands.fetch());
+	return created.map(command => ({
+		id: command.id,
+		name: command.name,
+		description: command.description
+	}));
 }
 
 // Register Slash Commands on CI
 // Specify the guild snowflake to instantly deploy commands on the specified server.
 // Otherwise, global commands can take up to an hour to roll out.
 export async function registerSlashCommands(guild?: `${bigint}`): Promise<void> {
-    // Duplicate command metadata if they register any aliases
-    const commands = classes
-        .map(command =>
-            command.aliases
-                .map(alias => ({
-                    ...command.meta,
-                    name: alias,
-                    description: `Alias for \`${command.meta.name}\`. ${command.meta.description}`
-                }))
-                .concat(command.meta)
-        )
-        .flat();
-    console.log("Generated command metadata:");
-    console.log(commands);
-    // Create a client object just to interact with the API
-    const client = new Client({ intents: [] });
-    // Because we're not using the object as intended to log into the gateway, we must manually
-    // create this object so that we can use its interface to call the Discord Slash Commands API
-    client.application = new ClientApplication(client, {});
-    // Implicitly use DISCORD_TOKEN
-    const application = await client.application.fetch();
-    console.log(application.id);
-    console.log(application.name);
-    console.log(application.description);
-    // Server commands deploy instantly, but global commands may take up to an hour to roll out
-    const created =
-        guild === undefined
-            ? await deployCommands(application, commands)
-            : await deployCommands(await client.guilds.fetch(guild), commands);
-    console.log("Created Slash Commands:");
-    console.log(created);
+	// Duplicate command metadata if they register any aliases
+	const commands = classes
+		.map(command =>
+			command.aliases
+				.map(alias => ({
+					...command.meta,
+					name: alias,
+					description: `Alias for \`${command.meta.name}\`. ${command.meta.description}`
+				}))
+				.concat(command.meta)
+		)
+		.flat();
+	console.log("Generated command metadata:");
+	console.log(commands);
+	// Create a client object just to interact with the API
+	const client = new Client({ intents: [] });
+	// Because we're not using the object as intended to log into the gateway, we must manually
+	// create this object so that we can use its interface to call the Discord Slash Commands API
+	client.application = new ClientApplication(client, {});
+	// Implicitly use DISCORD_TOKEN
+	const application = await client.application.fetch();
+	console.log(application.id);
+	console.log(application.name);
+	console.log(application.description);
+	// Server commands deploy instantly, but global commands may take up to an hour to roll out
+	const created =
+		guild === undefined
+			? await deployCommands(application, commands)
+			: await deployCommands(await client.guilds.fetch(guild), commands);
+	console.log("Created Slash Commands:");
+	console.log(created);
 }
---
+++ src/commands/link.ts
@@ -0,0 +1,104 @@
+import { ApplicationCommandData, CommandInteraction } from "discord.js";
+import { injectable } from "tsyringe";
+import { Command } from "../Command";
+import { getLogger, Logger } from "../logger";
+
+@injectable()
+export class LinkCommand extends Command {
+	#logger = getLogger("command:link");
+
+	static links: Record<string, { name: string; result: string }> = {
+		lftcg: {
+			name: "TCG Banlist",
+			result: "https://www.yugioh-card.com/uk/limited/"
+		},
+		lfocg: {
+			name: "OCG Banlist",
+			result: "https://www.yugioh-card.com/my/event/rules_guides/forbidden_cardlist.php?lang=en"
+		},
+		lfko: {
+			name: "Korean Banlist",
+			result: "http://yugioh.co.kr/site/limit_regulation.php"
+		},
+		dubsum: {
+			name: "Double Summon List",
+			result: "https://ygorganization.com/doublesummonlist/"
+		},
+		forget: {
+			name: "Forgetting",
+			result: "https://ygorganization.com/learnrulingspart13/"
+		},
+		dmgstep: {
+			name: "Damage Step",
+			result: "https://www.yugioh-card.com/uk/gameplay/damage.html\nhttp://yugipedia.com/wiki/Damage_Step#Cards_and_effects_that_can_be_activated"
+		},
+		linksum: {
+			name: "Link Summons",
+			result: "https://cdn.discordapp.com/attachments/377682286394736650/690868673157791774/unknown.png"
+		},
+		fetc: {
+			name: "Fast Effect Timing Chart",
+			result: "https://img.yugioh-card.com/en/gameplay/images/T-Flowchart_EN-US.jpg"
+		},
+		sumneg: {
+			name: "Summon Negation Timing",
+			result: "https://puu.sh/CLinf/3332459d3c.png"
+		},
+		atkdef: {
+			name: "ATK/DEF Modification",
+			result: "https://ygorganization.com/atk-def-modification-and-you/"
+		},
+		nomi: {
+			name: "Special Summon Monsters",
+			result: "https://cdn.discordapp.com/attachments/184324960842416129/680508513105346659/nomi_monsters.png"
+		},
+		rivgoz: {
+			name: "Rivalry/Gozen Rulings",
+			result: "https://img.yugioh-card.com/ygo_cms/ygo/all/uploads/CardFAQ_Rivalry-of-Warlords_Gozen-Match-1.pdf"
+		}
+	};
+
+	static override get meta(): ApplicationCommandData {
+		return {
+			name: "link",
+			description: "Display one of several links with useful information.",
+			options: [
+				{
+					type: "STRING",
+					name: "key",
+					description: "The name of the link you want to display.",
+					required: true,
+					choices: Object.keys(LinkCommand.links).map(k => {
+						return {
+							name: LinkCommand.links[k].name,
+							value: k
+						};
+					})
+				}
+			]
+		};
+	}
+
+	static override get aliases(): string[] {
+		return [];
+	}
+
+	protected override get logger(): Logger {
+		return this.#logger;
+	}
+
+	protected override async execute(interaction: CommandInteraction): Promise<number> {
+		const key = interaction.options.getString("key", true);
+		const content = LinkCommand.links[key].result;
+		await interaction.reply(content); // Actually returns void
+		const reply = await interaction.fetchReply();
+		// return latency
+		if ("createdTimestamp" in reply) {
+			const latency = reply.createdTimestamp - interaction.createdTimestamp;
+			return latency;
+		} else {
+			const latency = Number(reply.timestamp) - interaction.createdTimestamp;
+			return latency;
+		}
+	}
+}
