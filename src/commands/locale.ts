c2831163be81cbbdab4b4cdff623cafdd144b9b0
kevinlul <6320810+kevinlul@users.noreply.github.com>
2022-01-19 21:52:16 -0500

    Prototype command interface

--- src/commands/locale.ts
+++ src/commands/locale.ts
@@ -5,6 +5,7 @@ import { injectable } from "tsyringe";
 import { Command } from "../Command";
 import { getLogger, Logger } from "../logger";
 import { Metrics } from "../metrics";
+import { replyLatency } from "../utils";
 
 @injectable()
 export class LocaleCommand extends Command {
@@ -15,7 +16,37 @@ export class LocaleCommand extends Command {
 	}
 
 	static override get meta(): RESTPostAPIApplicationCommandsJSONBody {
-		return new SlashCommandBuilder().setName("locale").setDescription("Check locale for this channel.").toJSON();
+		return new SlashCommandBuilder()
+			.setName("locale")
+			.setDescription("Check or set Bastion's locale for this channel or server.")
+			.addSubcommand(subcommand => subcommand.setName("get").setDescription("Check Bastion's locale setting."))
+			.addSubcommand(subcommand =>
+				subcommand
+					.setName("set")
+					.setDescription("Override Bastion's locale for this channel or server.")
+					.addStringOption(option =>
+						option
+							.setName("scope")
+							.setDescription("Edit just this channel or the whole server?")
+							.addChoices([
+								["channel", "channel"],
+								["server", "server"]
+							])
+							.setRequired(true)
+					)
+					.addStringOption(option =>
+						option
+							.setName("locale")
+							.setDescription("The new default language to use in this channel or server.")
+							.setRequired(true)
+							.addChoice("English", "en")
+							.addChoice("Français", "fr")
+							.addChoice("Deutsch", "de")
+							.addChoice("Italiano", "it")
+							.addChoice("Português", "pt")
+					)
+			)
+			.toJSON();
 	}
 
 	protected override get logger(): Logger {
@@ -23,10 +54,30 @@ export class LocaleCommand extends Command {
 	}
 
 	protected override async execute(interaction: CommandInteraction): Promise<number> {
+		let content: string;
+		if (interaction.options.getSubcommand() === "get") {
+			content = `Your locale: ${interaction.locale}\nServer locale: ${interaction.guildLocale}`;
+		} else {
+			// subcommand set
+			const locale = interaction.options.getString("locale");
+			if (interaction.guildId === null) {
+				// direct message, ignore scope
+				content = `Locale for this direct message overridden with ${locale}. Your Discord setting is ${interaction.locale}.`;
+			} else {
+				const scope = interaction.options.getString("scope");
+				if (scope === "channel") {
+					content = `Locale for current channel ${interaction.channel} overridden with ${locale}. Server-wide setting is ${interaction.guildLocale}.`;
+				} else {
+					// server-wide
+					content = `Locale for this server overriden with ${locale}. Server-wide default for community servers is ${interaction.guildLocale}.`;
+				}
+			}
+		}
 		const reply = await interaction.reply({
-			content: `Your locale: ${interaction.locale}\nServer locale: ${interaction.guildLocale}`,
+			content,
+			ephemeral: true,
 			fetchReply: true
 		});
-		return "createdTimestamp" in reply ? reply.createdTimestamp - interaction.createdTimestamp : -1;
+		return replyLatency(reply, interaction);
 	}
 }
