2e0b1c0d41f2b6a1c2b804f79e7ccf0e0426c927
kevinlul <6320810+kevinlul@users.noreply.github.com>
2022-10-16 11:17:29 -0400

    /ygoprodeck: prototype autocomplete
    #198

--- src/commands/ygoprodeck.ts
+++ src/commands/ygoprodeck.ts
@@ -1,17 +1,17 @@
 import { SlashCommandBuilder, SlashCommandStringOption } from "@discordjs/builders";
 import { RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types/v10";
-import { ChatInputCommandInteraction } from "discord.js";
+import { AutocompleteInteraction, ChatInputCommandInteraction } from "discord.js";
 import fetch from "node-fetch";
 import { inject, injectable } from "tsyringe";
 import { c, t, useLocale } from "ttag";
-import { Command } from "../Command";
+import { AutocompletableCommand } from "../Command";
 import { buildLocalisedCommand, LocaleProvider } from "../locale";
 import { getLogger, Logger } from "../logger";
 import { Metrics } from "../metrics";
 import { editLatency } from "../utils";
 
 @injectable()
-export class YGOPRODECKCommand extends Command {
+export class YGOPRODECKCommand extends AutocompletableCommand {
 	#logger = getLogger("command:ygoprodeck");
 
 	constructor(metrics: Metrics, @inject("LocaleProvider") private locales: LocaleProvider) {
@@ -25,9 +25,9 @@ export class YGOPRODECKCommand extends Command {
 			() => c("command-description").t`Search the YGOPRODECK card database.`
 		);
 		const option = buildLocalisedCommand(
-			new SlashCommandStringOption().setRequired(true),
+			new SlashCommandStringOption().setRequired(true).setAutocomplete(true),
 			() => c("command-option").t`term`,
-			() => c("command-option-description").t`The name or password of the card you're looking for.`
+			() => c("command-option-description").t`The English name of the card you're looking for.`
 		);
 		builder.addStringOption(option);
 		return builder.toJSON();
@@ -37,14 +37,22 @@ export class YGOPRODECKCommand extends Command {
 		return this.#logger;
 	}
 
-	private async search(term: string): Promise<string> {
-		const url = new URL("https://db.ygoprodeck.com/carddbsearch_name.php");
-		url.searchParams.set("term", term);
+	private async search(term: string): Promise<string | { name: string; data: number }[]> {
+		const url = new URL("https://ygoprodeck.com/api/autocomplete.php");
+		url.searchParams.set("query", term);
 		const response = await (await fetch(url)).json();
 		if ("error" in response) {
 			return response.error;
 		} else {
-			return `https://db.ygoprodeck.com/card/?search=${encodeURIComponent(response.name)}`;
+			return response.suggestions;
+		}
+	}
+
+	async autocomplete(interaction: AutocompleteInteraction): Promise<void> {
+		const term = interaction.options.getFocused();
+		const suggestions = await this.search(term);
+		if (Array.isArray(suggestions)) {
+			await interaction.respond(suggestions.map(({ name }) => ({ name, value: name })).slice(0, 25));
 		}
 	}
 
@@ -53,7 +61,10 @@ export class YGOPRODECKCommand extends Command {
 		const lang = await this.locales.get(interaction);
 		useLocale(lang);
 		await interaction.reply(t`Searching YGOPRODECK for \`${term}\`…`);
-		const result = await this.search(term);
+		const response = await this.search(term);
+		const result = Array.isArray(response)
+			? `https://ygoprodeck.com/card/?search=${encodeURIComponent(response?.[0]?.data)}`
+			: response;
 		const reply = await interaction.editReply(result);
 		return editLatency(reply, interaction);
 	}
