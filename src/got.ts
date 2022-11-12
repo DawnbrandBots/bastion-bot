bca36d9691df5116c208f665ee2cd45b40e46cf2
kevinlul <6320810+kevinlul@users.noreply.github.com>
2022-11-12 12:41:31 -0500

    Add HTTP/1.1 keepAlive agents

--- src/got.ts
+++ src/got.ts
@@ -1,9 +1,18 @@
 import got, { Got } from "got";
 import { version } from "got/package.json";
+import { Agent as HTTPAgent } from "http";
+import { Agent as HTTPSAgent } from "https";
 
 export default function createGotClient(): Got {
-	// No cache for now because it has potentially unbounded memory use
+	// Cache should be managed by each use case
 	return got.extend({
+		// These are for HTTP/1.1
+		agent: {
+			// https://github.com/sindresorhus/got/issues/815 https://nodejs.org/api/http.html#http_class_http_agent
+			http: new HTTPAgent({ keepAlive: true }),
+			// https://nodejs.org/api/https.html#https_class_https_agent
+			https: new HTTPSAgent({ keepAlive: true })
+		},
 		http2: true,
 		headers: {
 			"User-Agent": `Bastion/${process.env.BOT_REVISION} (https://github.com/DawnbrandBots/bastion-bot) got/${version} (https://github.com/sindresorhus/got) Node.js ${process.version}`
