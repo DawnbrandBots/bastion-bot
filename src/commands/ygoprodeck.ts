c9c35fbea659be5c67d5414b6a48905aa5cebe0e
kevinlul <6320810+kevinlul@users.noreply.github.com>
2022-11-12 14:49:51 -0500

    add caching to /ygoprodeck autocomplete with mnemonic

--- src/commands/ygoprodeck.ts
+++ src/commands/ygoprodeck.ts
@@ -2,6 +2,7 @@ import { SlashCommandBuilder, SlashCommandStringOption } from "@discordjs/builde
 import { RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types/v10";
 import { AutocompleteInteraction, ChatInputCommandInteraction, escapeMarkdown } from "discord.js";
 import { Got } from "got";
+import { LRUMap, LRUMapWithDelete } from "mnemonist";
 import { inject, injectable } from "tsyringe";
 import { c, t, useLocale } from "ttag";
 import { AutocompletableCommand } from "../Command";
@@ -15,6 +16,8 @@ type YGOPRODECKResponse = { error: string } | { suggestions: { name: string; dat
 @injectable()
 export class YGOPRODECKCommand extends AutocompletableCommand {
 	#logger = getLogger("command:ygoprodeck");
+	private httpCache = new LRUMapWithDelete<string, string>(10000);
+	private suggestionCache = new LRUMap<string, number>(20000);
 
 	constructor(
 		metrics: Metrics,
@@ -46,7 +49,10 @@ export class YGOPRODECKCommand extends AutocompletableCommand {
 	private async search(term: string): Promise<YGOPRODECKResponse> {
 		const url = new URL("https://ygoprodeck.com/api/autocomplete.php");
 		url.searchParams.set("query", term);
-		return await this.got(url, { headers: { Accept: "application/json" } }).json<YGOPRODECKResponse>();
+		return await this.got(url, {
+			cache: this.httpCache,
+			headers: { Accept: "application/json" }
+		}).json<YGOPRODECKResponse>();
 	}
 
 	async autocomplete(interaction: AutocompleteInteraction): Promise<void> {
@@ -57,8 +63,13 @@ export class YGOPRODECKCommand extends AutocompletableCommand {
 			const latency = Date.now() - start;
 			this.#logger.info(serialiseInteraction(interaction, { autocomplete: term, latency, response }));
 			if ("suggestions" in response) {
+				const options = [];
+				for (const { name, data } of response.suggestions) {
+					this.suggestionCache.set(name, data);
+					options.push({ name, value: name });
+				}
 				// Maximum 25 options https://discordjs.guide/slash-commands/autocomplete.html
-				await interaction.respond(response.suggestions.map(({ name }) => ({ name, value: name })).slice(0, 25));
+				await interaction.respond(options.slice(0, 25));
 			} else {
 				await interaction.respond([]);
 			}
@@ -72,19 +83,27 @@ export class YGOPRODECKCommand extends AutocompletableCommand {
 	protected override async execute(interaction: ChatInputCommandInteraction): Promise<number> {
 		const term = interaction.options.getString("term", true);
 		let content;
-		try {
-			const response = await this.search(term);
-			this.#logger.info(serialiseInteraction(interaction, { term, response }));
-			content =
-				"suggestions" in response
-					? `https://ygoprodeck.com/card/?search=${encodeURIComponent(response.suggestions[0]?.data)}`
-					: response.error;
-		} catch (error) {
-			this.#logger.warn(serialiseInteraction(interaction, { term }), error);
-			const lang = await this.locales.get(interaction);
-			useLocale(lang);
-			const searchTerm = escapeMarkdown(term);
-			content = t`Something went wrong searching YGOPRODECK for \`${searchTerm}\`.`;
+		const cached = this.suggestionCache.get(term);
+		if (cached) {
+			content = `https://ygoprodeck.com/card/?search=${encodeURIComponent(cached)}`;
+			this.#logger.info(serialiseInteraction(interaction, { term, cached }));
+		} else {
+			try {
+				const start = Date.now();
+				const response = await this.search(term);
+				const latency = Date.now() - start;
+				this.#logger.info(serialiseInteraction(interaction, { term, latency, response }));
+				content =
+					"suggestions" in response
+						? `https://ygoprodeck.com/card/?search=${encodeURIComponent(response.suggestions[0]?.data)}`
+						: response.error;
+			} catch (error) {
+				this.#logger.warn(serialiseInteraction(interaction, { term }), error);
+				const lang = await this.locales.get(interaction);
+				useLocale(lang);
+				const searchTerm = escapeMarkdown(term);
+				content = t`Something went wrong searching YGOPRODECK for \`${searchTerm}\`.`;
+			}
 		}
 		const reply = await interaction.reply({ content, fetchReply: true });
 		return replyLatency(reply, interaction);
