43979af95cf0ab7e529c25602f9153093f78c85b
kevinlul <6320810+kevinlul@users.noreply.github.com>
2023-02-08 00:08:24 -0500

    Verify constraints on Slash Command data

--- test/locale.spec.ts
+++ test/locale.spec.ts
@@ -1,8 +1,25 @@
-import { loadTranslations } from "../src/locale";
+import { classes } from "../src/commands";
+import { COMMAND_LOCALIZATIONS, loadTranslations } from "../src/locale";
 
 describe(".po files", () => {
-	test("syntax valid", () => {
+	test("syntax is valid", () => {
 		const locales = loadTranslations();
-		expect(locales.length).toBeGreaterThan(0);
+		expect(locales.length).toBe(COMMAND_LOCALIZATIONS.length);
+		for (const locale of COMMAND_LOCALIZATIONS.map(l => l.gettext)) {
+			expect(locales).toContain(locale);
+		}
+	});
+	test("command metadata is valid", () => {
+		loadTranslations();
+		const commands = classes.map(command => command.meta);
+		expect(commands.length).toBeGreaterThan(0);
+		let translationsLoaded = false;
+		for (const command of commands) {
+			if (command.description_localizations?.ko !== command.description_localizations?.ja) {
+				translationsLoaded = true;
+				break;
+			}
+		}
+		expect(translationsLoaded);
 	});
 });
