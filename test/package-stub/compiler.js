16ea85b4da190d8001682c47e35b12787321307d
kevinlul <6320810+kevinlul@users.noreply.github.com>
2024-12-01 11:18:05 -0500

    Patch more ttag-cli transitive dependencies
    Fixes #551

---
+++ test/package-stub/compiler.js
@@ -0,0 +1,2 @@
+// Stubs out https://github.com/ttag-org/ttag-cli/blob/6fa84654d4f0be3d0369dc72c81261950c368d90/src/lib/extract.ts#L8,
+// which is unused for this project since we're not using Svelte.
